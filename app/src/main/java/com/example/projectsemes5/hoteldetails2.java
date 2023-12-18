f035afa7e49c45c768aac39287f9c082b60e966e
Robertus <ferdinandrobertus@gmail.com>
2023-12-18 09:40:47 +0700

    Robert Push

--- app/src/main/java/com/example/projectsemes5/hoteldetails2.java
+++ app/src/main/java/com/example/projectsemes5/hoteldetails2.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.ScrollView;
