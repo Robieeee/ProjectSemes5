f4c0e5414bad992c8d3da85d694328f1a1f7e91a
Robertus <ferdinandrobertus@gmail.com>
2023-12-18 10:17:07 +0700

    Robert Push

--- app/src/main/java/com/example/projectsemes5/chatActivity.java
+++ app/src/main/java/com/example/projectsemes5/chatActivity.java
@@ -2,13 +2,27 @@ package com.example.projectsemes5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class chatActivity extends AppCompatActivity {
+    ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        back = findViewById(R.id.Buttonback);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(chatActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
